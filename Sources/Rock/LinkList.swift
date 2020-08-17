553e661c5f99b3e57f8299b42fd6d722c4a5b9bc
wenluma <wenluma_521@163.com>
2020-08-17 09:16:14 +0800

    添加 Node

---
+++ Sources/Rock/LinkList.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  
+//
+//  Created by miao gaoliang on 2020/8/16.
+//
+
+class Node<T> {
+  var value: T?
+  var next: Node<T>?
+  weak var pre: Node<T>?
+  required init(value: T?, next: Node<T>? = nil, pre: Node<T>? = nil) {
+    self.value = value
+    self.next = next
+    self.pre = pre
+  }
+}
